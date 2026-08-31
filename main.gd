c1eb1861bd4697543ce1a387d7e69e83acf203dc
Mathiaskorre <mathiaskorre@gmail.com>
2026-08-31 13:55:12 +0200

    Github test

--- main.gd
+++ main.gd
@@ -4,9 +4,10 @@ var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	health += 1000000
 	print(health)
 
 func _input(event):
 	if event.is_action_pressed("my_action"):
-		health -= 20 
+		health -= 20
 		print(health)
